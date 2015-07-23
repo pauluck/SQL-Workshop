75614208f1916b6509f940d85398b34b75101029
pauluck <auluckpuneet@gmail.com>
2015-07-22 22:02:14 -0400

    Week 4 Assignment

---
+++ week4_pauluck_SQL.sql
@@ -0,0 +1,43 @@
+﻿/* Puneet Auluck - SQL Assignment #4 */
+
+/* Please create an organization chart for a real or imagined organization, implemented in a single SQL table.  */
+
+/* 1. Create the table. Each row should minimally include the person’s name, the person’s supervisor, and the
+person’s job title. Using ID columns is encouraged. */
+
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees (
+  emp_id	integer PRIMARY KEY NOT NULL,
+  first_name	varchar(50) NOT NULL,
+  last_name	varchar(50) NOT NULL,
+  title		varchar(100) NOT NULL,
+  supervisor_id	integer
+);
+
+
+/* 2. Populate the table with a few sample rows.*/
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (101, 'John', 'Smith', 'Deputy Commissioner', null);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (102, 'Monica', 'Bing', 'Assistant Commissioner',101);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (103, 'Zehra', 'Holiak', 'Director', 102);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (104, 'Fred', 'Russo', 'Director', 102);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (105, 'Sameer', 'Anki', 'Division Chief', 103);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (106, 'Jai', 'Maher', 'Division Chief', 104);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (107, 'Sumika', 'Jas', 'Section Chief', 105);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (108, 'Edward', 'Zing', 'Section Chief', 105);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (109, 'Allen', 'Chu', 'Section Chief', 106);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (110, 'Jane', 'Miller', 'Inventory Specialist', 107);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (111, 'Laura', 'Riccio', 'Report Sepcialist', 108);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (112, 'Ron', 'Dillon', 'Database Specialist', 108);
+INSERT INTO employees (emp_id, first_name, last_name, title, supervisor_id) VALUES (113, 'Jason', 'Aiden', 'Programmer', 109);
+
+
+/*3. Provide a single SELECT statement that displays the information in the table, showing who reports to whom */
+SELECT e.emp_id AS "Employee ID", 
+  e.first_name AS "First Name", 
+  e.last_name AS "Last Name", 
+  e.title AS "Title", 
+  e2.first_name || ' ' || e2.last_name AS "Supervisor"
+ FROM employees e LEFT JOIN employees e2 
+ ON e.supervisor_id=e2.emp_id;
+
---
+++ worktravel.sql
@@ -0,0 +1,37 @@
+﻿DROP TABLE IF EXISTS Travel;
+DROP TABLE IF EXISTS Routes;
+
+CREATE TABLE Routes (
+	RouteId 	INTEGER PRIMARY KEY NOT NULL,
+	Method		VARCHAR(20),
+	TravelTime	INTEGER NOT NULL,
+	TravelCost	NUMERIC NOT NULL
+);
+
+CREATE TABLE Travel (
+	TravelDate 	DATE NOT NULL,
+	TravelType	VARCHAR(20),
+	RouteId		INTEGER NOT NULL,
+	WaitTime 	INTEGER,
+	TrafficTime 	INTEGER
+);
+
+
+INSERT INTO Routes (RouteId, Method, TravelTime, TravelCost) VALUES (111, 'Bus-Trains', 95, 6.75);
+INSERT INTO Routes (RouteId, Method, TravelTime, TravelCost) VALUES (112, 'Train-Ferry-Train', 125, 2.75);
+INSERT INTO Routes (RouteId, Method, TravelTime, TravelCost) VALUES (113, 'Car', 80, 20.00);
+
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/6/2015','Leaving', 111, 10, 0);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/6/2015','Arriving', 111, 15, 15);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/7/2015','Leaving', 113, 0, 0);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/7/2015','Arriving', 113, 0, 30);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/8/2015','Leaving', 111, 0, 0);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/8/2015','Arriving', 111, 20, 20);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/9/2015','Leaving', 113, 0, 10);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/9/2015','Arriving', 113, 0, 20);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/9/2015','Leaving', 112, 5, 0);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/9/2015','Arriving', 111, 5, 15);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/10/2015','Leaving', 111, 5, 0);
+INSERT INTO Travel (TravelDate, TravelType, RouteId, WaitTime, TrafficTime) VALUES ('7/10/2015','Arriving', 112, 20, 0);
+
+select * from travel left join routes on travel.routeid=routes.routeid;
